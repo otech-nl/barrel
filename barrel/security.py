e11618c8c81af2157d0dceb24afb6f41a957abbb
steets@otech.nl <steets@otech.nl>
2017-04-06 12:07:22 +0200

    Refactored security

--- barrel/security.py
+++ barrel/security.py
@@ -1,11 +1,21 @@
-from flask_security import Security, SQLAlchemyUserDatastore, UserMixin, utils
+from flask_security import Security, SQLAlchemyUserDatastore, RoleMixin, UserMixin, utils
 from sqlalchemy.ext.hybrid import hybrid_property
 
 ########################################
 
 
-def bootstrap(app):
+def bootstrap(app):  # noqa: C901  too complex
     db = app.db
+
+    class Role(db.BaseModel, RoleMixin):
+        __abstract__ = True
+
+        name = db.Column(db.String(80), unique=True)
+
+        def __repr__(self):
+            return self.name
+
+    db.Role = Role
 
     class User(db.BaseModel, UserMixin):
         __abstract__ = True
@@ -31,11 +41,45 @@ def bootstrap(app):
         def __repr__(self):
             return self.email
 
+        @hybrid_property
+        def role(self):
+            return self.roles[0]
+
+        def has_access(self, model):
+            ' (dummy) returns true if user has access to model '
+            return self.company_id == model.get_company_id()
+
+        def get_permission(self, model=None):
+            ' returns either ro (read only), rw (read write) or - (none)'
+            if self.has_role('admin'):
+                return 'rw'
+            elif self.has_role('mod'):
+                if model:
+                    if self.has_access(model):
+                        return 'rw'
+                    else:
+                        return '-'
+                else:
+                    return 'rw'
+            elif self.has_role('bestuur'):
+                if not model or self.has_access(model):
+                    return 'ro'
+                else:
+                    return '-'
+
     db.User = User
 
 
-def enable(app, user_class, role_class=None):
+def enable(app, user_class, role_class):
     app.logger.info('Enabling security')
+    db = app.db
+    user_roles = db.Table('user_roles',
+                          db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
+                          db.Column('role_id', db.Integer(), db.ForeignKey('role.id')))
+    user_class.roles = db.relationship(
+        'Role',
+        secondary=user_roles,
+        backref=db.backref('users', lazy='dynamic'))
     user_datastore = SQLAlchemyUserDatastore(app.db, user_class, role_class)
     app.security = Security(app, user_datastore)
     app.security.user_datastore = user_datastore
--- skeleton/models.py
+++ skeleton/models.py
@@ -1,12 +1,7 @@
 # coding=utf8
 from app import app
-from datetime import datetime
-from flask_security import RoleMixin, current_user
-from sqlalchemy.exc import SQLAlchemyError
-from sqlalchemy.ext.hybrid import hybrid_property
 from sqlalchemy.orm import backref
-from sqlalchemy.sql import func, desc
-from sqlalchemy.types import TypeDecorator
+from sqlalchemy.sql import func
 import barrel
 
 db = app.db
@@ -25,36 +20,16 @@ class BaseModel(db.BaseModel, db.CRUDMixin, app.forms.FormModelMixin):
 
 barrel.security.bootstrap(app)
 
-user_roles = db.Table('user_roles',
-                      db.Column('user_id', db.Integer(), db.ForeignKey('user.id')),
-                      db.Column('role_id', db.Integer(), db.ForeignKey('role.id')))
-
-
-class Role(BaseModel, RoleMixin):
-    name = db.Column(db.String(80), unique=True)
-
+class Role(db.Role, db.CRUDMixin, app.forms.FormModelMixin):
     def get_company_id(self):
         return Company.get_admin_company().id
 
-    def __repr__(self):
-        return self.name
-
-########################################
-
 
 class User(db.User, db.CRUDMixin):
-    roles = db.relationship(
-        'Role',
-        secondary=user_roles,
-        backref=db.backref('users', lazy='dynamic'))
 
     company_id = db.Column(db.Integer, db.ForeignKey('company.id'), nullable=False)
     company = db.relationship("Company",
                               backref=backref("users", cascade='save-update, merge, delete'))
-
-    @hybrid_property
-    def role(self):
-        return self.roles[0]
 
     def get_company_id(self):
         return self.company_id
@@ -62,24 +37,7 @@ class User(db.User, db.CRUDMixin):
     def has_access(self, model):
         return self.company_id == model.get_company_id()
 
-    def get_permission(self, model=None):
-        if self.has_role('admin'):
-            return 'rw'
-        elif self.has_role('mod'):
-            if model:
-                if self.has_access(model):
-                    return 'rw'
-                else:
-                    return '-'
-            else:
-                return 'rw'
-        elif self.has_role('bestuur'):
-            if not model or self.has_access(model):
-                return 'ro'
-            else:
-                return '-'
-
-barrel.security.enable(app, user_class=User, role_class=Role)
+barrel.security.enable(app, User, Role)
 
 ########################################
 
