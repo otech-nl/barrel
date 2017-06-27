9ddb485c87ca3c3000aa1dec1ae80ba8408aaaee
steets@otech.nl <steets@otech.nl>
2017-06-27 16:11:07 +0200

    Secured admin

--- barrel/admin.py
+++ barrel/admin.py
@@ -1,4 +1,9 @@
 """ Wrapper around Flask-Admin """
+import flask
+from flask_admin.contrib import sqla
+from flask_security import current_user
+
+
 try:
     from flask_admin import Admin
     from flask_admin.contrib.sqla import ModelView
@@ -9,8 +14,31 @@ except ImportError as e:
 ########################################
 
 
-def enable(app, models):
+class SecureModelView(sqla.ModelView):
+
+    def is_accessible(self):
+        return current_user.is_active and current_user.is_authenticated and current_user.has_role('admin')
+
+    def _handle_view(self, name, **kwargs):
+        """
+        Override builtin _handle_view in order to redirect users when a view is not accessible.
+        """
+        if not self.is_accessible():
+            if current_user.is_authenticated:
+                # permission denied
+                flask.abort(403)
+            else:
+                # login
+                return flask.redirect(flask.url_for('security.login', next=flask.request.url))
+
+
+########################################
+
+
+def enable(app, models, model_view=SecureModelView):
     """ Anable Flask-Admin.
+
+    Available as app.admin.
 
     Args:
         app (Flask app): app.admin is set
@@ -19,15 +47,15 @@ def enable(app, models):
     app.logger.info('Enabling admin interface')
     app.admin = Admin(app, name=app.name)
     for model in models:
-        add_model(app, model)
+        add_model(app, model, model_view)
     return app.admin
 
 
-def add_model(app, model):
+def add_model(app, model, model_view=SecureModelView):
     """ Add a model to Flask-Admin.
 
     Args:
         app (Flask app)
         model (SQLAlchemy model)
     """
-    app.admin.add_view(ModelView(model, app.db.session))
+    app.admin.add_view(model_view(model, app.db.session))
